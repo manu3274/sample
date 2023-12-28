1288c4540b3210cd1c01d23b3ef57be8be6fea42
manu <ll3274ll3274ll@gmail.com>
2023-12-28 23:23:37 +0900

    solution

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "abc",
+            "def",
+            "ghi"});
+            this.checkedListBox1.Location = new System.Drawing.Point(25, 33);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(265, 88);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+
+
+        public void Start()
+        {
+            var options = new JsonSerializerOptions
+            {
+                // 日本語をエスケープしない
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                // コメントを許可
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                // 末尾のコンマを許可
+                AllowTrailingCommas = true
+            };
+
+            Dictionary<string, Dictionary<string, string>> conf = null;
+            using (FileStream fs = new FileStream(System.AppDomain.CurrentDomain.BaseDirectory + "config\\config.json", FileMode.Open, FileAccess.Read))
+            {
+                conf =JsonSerializer.Deserialize<Dictionary<string, Dictionary<string,string>>>(fs, options);
+            };
+
+            var param = conf["test"];
+            Console.WriteLine(param["4"]);
+        }
+    }
+}
